695b9dfa6a29d6017ef42b2efe597f10cbde413d
mzoric33 <zoricmarin4@gmail.com>
2022-08-31 14:14:52 +0200

    Cleaned controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,7 +9,6 @@ import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { async } from 'regenerator-runtime';
 
 const controlRecipes = async function () {
   try {
