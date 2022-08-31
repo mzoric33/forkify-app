726c02c89bffff99ef80f45ae6a7ce29e119d45d
mzoric33 <zoricmarin4@gmail.com>
2022-08-31 12:30:50 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b29e1c1e-0978-45ba-bfbd-67632a4a4d4a';
+export const MODAL_CLOSE_SEC = 2.5;
