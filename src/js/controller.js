463e01809e47455f7615e0bf8e73b4e44c302e06
mzoric33 <zoricmarin4@gmail.com>
2022-08-31 13:04:49 +0200

    Deleted new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,10 +118,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -130,6 +126,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
